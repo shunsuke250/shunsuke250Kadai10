786f94a4ee56aa7220f5d3f0621248b2364d7773
shunsuke250 <soeyamashunsuke@gmail.com>
2023-09-28 21:25:29 +0900

    :sparkles: TableViewCellファイルを作成

---
+++ PrefectureList23-09-28/PrefectureList23-09-28/PrefectureTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PrefectureTableViewCell.swift
+//  PrefectureList23-09-28
+//
+//  Created by 副山俊輔 on 2023/09/28.
+//
+
+import UIKit
+
+class PrefectureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
